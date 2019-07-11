dc01504db0c4fb1d04dc743affaff224579724f4
OtaGran <ujt2h8@gmail.com>
2019-07-11 16:14:15 +0800

    feat : implement getSumOfEvens

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -3,41 +3,75 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        return IntStream.rangeClosed(Math.min(leftBorder, rightBorder), Math.max(leftBorder, rightBorder))
+                .filter(number -> number % 2 == 0)
+                .sum();
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+
+        int count = 0;
+        for (int i = leftBorder; i < rightBorder; i++) {
+            if (i % 2 != 0)
+                count += i;
+        }
+        return count;
+
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        int count = 0;
+        for (int i : arrayList) {
+
+            count += i;
+        }
+        return count + 2;
+
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) % 2 != 0)
+                arrayList.set(i, arrayList.get(i) * 3 + 2);
+        }
+        return arrayList;
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
+
         throw new NotImplementedException();
     }
 
     public double getMedianOfEven(List<Integer> arrayList) {
+
         throw new NotImplementedException();
     }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        int count = 0;
+        int sum = 0;
+        for (int i : arrayList) {
+            if (i % 2 == 0)
+                sum += i;
+            count++;
+        }
+        return sum / count;
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        throw new NotImplementedException();
+        return arrayList.indexOf(specialElment) % 2 == 0;
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
+
+
         throw new NotImplementedException();
     }
 
